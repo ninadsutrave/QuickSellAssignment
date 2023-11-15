f7cd904527f69bad77936ed80bec6d99deb4e146
Ninad Sutrave <ninadsutrave@gmail.com>
2023-11-16 01:03:37 +0530

    card styles added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
+import PropTypes from 'prop-types'
 import './Card.css'
 
-const Card = () => {
+const Card = ({ticketId, ticketTitle, available}) => {
+
+   // id, title, status, priority, userName, available
+  const userStatusIconColor = available?'green':'grey';
+
   return (
-    <div>Card</div>
+    <div className="card-wrapper">
+        <div className="card-upper-wrapper">
+            <div className="ticket-details">
+                <span className="ticket-id">{ticketId}</span>
+                <div className="user-container">
+                    <img/>
+                    <div className="user-status-circle" style={{backgroundColor: userStatusIconColor}}></div>
+                </div>
+            </div>
+            <div className="ticket-title-span">
+                <img/>
+                <h2 className="ticket-title">{ticketTitle}</h2>
+            </div>
+        </div>
+        <div className="card-lower-wrapper">
+            <div className="priority-icon-wrapper"><img/></div>
+            <div className="tag-wrapper">
+                <img/>
+                <div className="tag-name">Feature Request</div>
+            </div>
+        </div>
+    </div>
   )
 }
 
+Card.propTypes = {
+    ticketId: PropTypes.string.isRequired,
+    ticketTitle: PropTypes.string.isRequired,
+    status: PropTypes.string.isRequired,
+    priority: PropTypes.number.isRequired,
+    userName: PropTypes.string.isRequired,
+    available: PropTypes.bool.isRequired
+};
+  
 export default Card
