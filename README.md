36eea7faecc43dd90fc8f34e851568f681bcfa74
Ninad Sutrave <ninadsutrave@gmail.com>
2023-11-16 14:39:42 +0530

    toggle button, fonts, favicon, readme updated

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
-# QuickSell Assignment
+<div align="center"><p align="center"><a href="https://quicksell.co/"><img src="./favicon.png" alt="QuickSell Logo" width="140px" /></a></p></div>
 
-Link: https://quicksell.notion.site/Frontend-assignment-fa2f7f8aaada4099916994bad6ec70c2
+<h1 align="center">QuickSell Frontend Assignment</h1>
+
+# Task
+A task tracking application featuring a Kanban Style Dashboard
+https://quicksell.notion.site/Frontend-assignment-fa2f7f8aaada4099916994bad6ec70c2
+
+## Details
+Name: Ninad Sutrave
+Institute: IIT Ropar
+Entry Number: 2019MED1010
+Portfolio: ninadsutrave.in
+Email: 2019med1010@iitrpr.ac.in
+
+## Libraries/Packages Used
+ - *React Icons*
+ For necesssary icons in the UI
+
+ - *Prop Types*
+ To specify prop types passed to the components
+
+ ## APIs used
+
+ *UI Avatars*
+ https://ui-avatars.com/api
+ To generate avatar icon for each user
+
+ ## Preview
--- src/components/ToggleButton.jsx
+++ src/components/ToggleButton.jsx
@@ -1,8 +1,63 @@
+import { useState } from 'react'
+import { useDisplayUpdate } from '../contexts/DisplayContext'
+import { BiSlider as SortIcon } from "react-icons/bi";
+import { MdKeyboardArrowDown as DownIcon } from "react-icons/md";
 import './ToggleButton.css'
 
 const ToggleButton = () => {
+
+    const [optionsMenu, setOptionsMenu] = useState(false)
+    const [groupingType, setGroupingType] = useState('status');
+    const [orderingType, setOrderingType] = useState('priority');
+    const toggleDisplay = useDisplayUpdate()
+
+    const handleMenu = (event) => {
+        const tagName = event.target.tagName.toLowerCase();
+        if(tagName != "select") {
+            setOptionsMenu(!optionsMenu)
+        }
+    }
+
+    const handleGroupingChange = (event) => {
+        setGroupingType(event.target.value);
+        toggleDisplay(event.target.value, orderingType)
+    };
+    
+    const handleOrderingChange = (event) => {
+        setOrderingType(event.target.value);
+        toggleDisplay(groupingType, event.target.value)
+    };
+
   return (
-    <div>ToggleButton</div>
+    <button className="toggle-button" onClick={handleMenu}>
+        <div className="toggle-button-wrapper">
+            <SortIcon />
+            <strong>Display</strong>
+            <DownIcon />
+        </div>
+        
+        {
+            optionsMenu &&
+            <div className="options-menu">
+                <div className="menu-option">
+                    <label className="dropdown-label">Grouping</label>
+                    <select value={groupingType} onChange={handleGroupingChange}>
+                        <option value="status">Status</option>
+                        <option value="user">User</option>
+                        <option value="priority">Priority</option>
+                    </select>
+                </div>
+            
+                <div className="menu-option">
+                    <label className="dropdown-label">Ordering</label>
+                    <select value={orderingType} onChange={handleOrderingChange}>
+                        <option value="priority">Priority</option>
+                        <option value="title">Title</option>
+                    </select>
+                </div>
+            </div>
+        }
+    </button>
   )
 }
 
